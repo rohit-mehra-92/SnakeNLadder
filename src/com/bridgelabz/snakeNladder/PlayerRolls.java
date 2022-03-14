1d90b524a42d44b1550d8a96a953b682ca011b20
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-14 12:04:49 +0530

    player start with zero

---
+++ src/com/bridgelabz/snakeNladder/PlayerRolls.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeNladder;
+
+public class PlayerRolls{
+    int value = (int) (Math.random()*7);
+    int rollDice(){
+        return value;
+    }
+
+}
+
---
+++ src/com/bridgelabz/snakeNladder/SnakeNLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeNladder;
+
+public class SnakeNLadder {
+    public static void main(String[] args) {
+
+        int playerPosition = 0;
+        System.out.println("player starts with " + playerPosition);
+
+    }
+}
