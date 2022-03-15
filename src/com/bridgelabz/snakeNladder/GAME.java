a408dc28d0a9134a00aae26ab8b16cba90802885
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-16 00:02:12 +0530

    Added:Ensured that player needs to get position 100 in order to win

--- src/com/bridgelabz/snakeNladder/GAME.java
+++ src/com/bridgelabz/snakeNladder/GAME.java
@@ -6,35 +6,43 @@ public class GAME {
     private static final int NO_PLAY = 0;
     final int WIN = 100;
     int position = 0;
+    int previousPosition = 0;
+
+    public int dieRoll() {
+        return (int) (Math.random() * 6) + 1;
+    }
 
     void StartGame() {
-        while (WIN > position) {
+        while (position < WIN) {
+            previousPosition = position;
+         System.out.println(" previous position " + previousPosition);
+
             int num = (int) Math.floor(Math.random() * 10) % 3;
             int value = dieRoll();
-            System.out.println("player Dice rolls: " + value);
+          System.out.println("player rolls dice and got : " + value);
             switch (num) {
                 case SNAKE:
-                    System.out.println("GOT SNAKE moves");
+                    //System.out.println("GOT SNAKE");
                     position = position - value;
-                    System.out.println("player position : " + position);
-                    break;
-
+                    if (position < 0) {
+                        position = 0;
+                        System.out.println("1st position : " + position);
+                        break;
+                    }
                 case LADDER:
-                    System.out.println("GOT LADDER");
+                    //System.out.println("GOT LADDER");
                     position = value + position;
-                    System.out.println("player position : " + position);
-                    break;
-
-                case NO_PLAY:
-                    System.out.println("PLAYER DOES NOT MOVE");
-                    System.out.println("player position : " + position);
-                    break;
+                    if (position > WIN) {
+                        position = previousPosition;
+                        System.out.println("2nd position :" + position);
+                        break;
+                    }
+                    else if (WIN == position){
+                        System.out.println("player Wins");
+                    }
 
             }
         }
     }
-
-    public static int dieRoll() {
-        return (int) (Math.random() * 7);
-    }
 }
+
--- src/com/bridgelabz/snakeNladder/SnakeNLadder.java
+++ src/com/bridgelabz/snakeNladder/SnakeNLadder.java
@@ -5,6 +5,7 @@ public class SnakeNLadder {
     public static void main(String[] args) {
     GAME game = new GAME();
     game.StartGame();
+
     }
 }
 
